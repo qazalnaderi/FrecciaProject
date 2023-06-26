68c243c48a89fd188844383ffffd0defcc144df8
qazalnaderi <ghazalnaderi4@gmail.com>
2023-06-26 12:31:02 +0330

    Image added.

--- Freccia/src/Main.java
+++ Freccia/src/Main.java
@@ -1,13 +1,20 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Main extends PApplet {
     public static PApplet processing;
+    public static PImage knight;
+    public static PImage knight2;
+    public static PImage injuredKnight;
+    public static PImage injuredKnight2;
 
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args) {PApplet.main("Main", args);}
     @Override
     public void setup() {
         processing = this;
+        knight = loadImage("knight.png");
+        injuredKnight = loadImage("injured-knight.png");
+        knight2 = loadImage("knight2.png");
     }
     @Override
     public void settings() {
