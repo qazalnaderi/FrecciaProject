ec5b395918bb1f861199e56e301574d5a621ac77
qazalnaderi <ghazalnaderi4@gmail.com>
2023-06-26 12:25:53 +0330

    Neccessary methods added.

--- Freccia/src/Main.java
+++ Freccia/src/Main.java
@@ -1,6 +1,22 @@
 import processing.core.PApplet;
 public class Main extends PApplet {
+    public static PApplet processing;
+
     public static void main(String[] args) {
 
     }
-}
+    @Override
+    public void setup() {
+        processing = this;
+    }
+    @Override
+    public void settings() {
+        size(500, 700);
+    }
+    @Override
+    public void draw() {}
+
+
+
+
+    }
